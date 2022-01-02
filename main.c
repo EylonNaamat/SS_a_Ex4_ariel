e2cd259063d33a16adddca63a88c54be213b4203
EylonNaamat <eylonnaamat@gmail.com>
2022-01-02 20:43:29 +0200

    main

--- main.c
+++ main.c
@@ -8,13 +8,7 @@
 int main()
 {
     char ch;
-    int size;
-    int num;
-    int num2;
     pnode graph;
-    pnode pn;
-    pnode pn2;
-    pedge pe;
     while(!feof(stdin))
     {
         scanf("%c", &ch);
@@ -22,32 +16,8 @@ int main()
         {
             deleteGraph_cmd(&graph);
             build_graph_cmd(&graph);
-            while(graph != NULL)
-            {
-                pnode p = graph;
-                graph = graph ->next;
-                free_node(p);
-            }
-            scanf("%d", &size);
-            graph = node_alloc(--size);
-            while(size > 0)
-            {
-                pnode temp = node_alloc(--size);
-                temp ->next = graph;
-                graph = temp;
-            }
         }
-        if(ch == 'n')
-        {
-            scanf("%d", &num);
-            pn = find_node(graph, num);
-            while(scanf("%d", &num))
-            {
-                scanf("%d", &num2);
-                pn2 = find_node(graph, num);
-                add_edge(edge_alloc(num2, pn2, NULL), pn);
-            }
-        }
+
         if(ch == 'B')
         {
             insert_node_cmd(&graph);
