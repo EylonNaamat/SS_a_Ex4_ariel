1c38138e990837efe03c1e3ecdbcc52328362c4f
EylonNaamat <eylonnaamat@gmail.com>
2022-01-02 18:22:14 +0200

    update

--- edge.c
+++ edge.c
@@ -2,11 +2,6 @@
 #include "node.h"
 #include <stdio.h>
 
-typedef struct edge_ {
-    int weight;
-    pnode endpoint;
-    struct edge_ *next;
-} edge, *pedge;
 
 pedge edge_alloc(int weight, pnode endpoint, pedge next)
 {
--- edge.h
+++ edge.h
@@ -1,6 +1,11 @@
 #include "node.h"
 
-typedef struct edge_ edge, *pedge;
+
+typedef struct edge_ {
+    int weight;
+    pnode endpoint;
+    struct edge_ *next;
+} edge, *pedge;
 
 
 pedge edge_alloc(int weight, pnode endpoint, pedge next);
