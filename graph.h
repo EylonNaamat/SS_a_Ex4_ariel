713e1185eb4c9ee7a4930e2f318702fd36365bff
michael <you@example.com>
2022-01-02 18:35:31 +0200

    start graph

--- graph.h
+++ graph.h
@@ -1,20 +1,11 @@
+#include "node.h"
+#include "edge.h"
 #ifndef GRAPH_
 #define GRAPH_
 
-typedef struct GRAPH_NODE_ *pnode;;
-
-typedef struct edge_ {
-    int weight;
-    pnode endpoint;
-    struct edge_ *next;
-} edge, *pedge;
-
-
-typedef struct GRAPH_NODE_ {
-    int node_num;
-    pedge edges;
-    struct GRAPH_NODE_ *next;
-} node, *pnode;
+typedef struct GRAPH_EM_{
+    pnode nodes;
+} graph, *graph;
 
 void build_graph_cmd(pnode *head);
 void insert_node_cmd(pnode *head);
