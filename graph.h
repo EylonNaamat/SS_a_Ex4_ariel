da0588e9b68dacdefdfb24b880e83edd73864de3
EylonNaamat <eylonnaamat@gmail.com>
2022-01-02 20:23:43 +0200

    bla

--- graph.h
+++ graph.h
@@ -1,11 +1,4 @@
 #include "node.h"
-#include "edge.h"
-#ifndef GRAPH_
-#define GRAPH_
-
-typedef struct GRAPH_EM_{
-    pnode nodes;
-} graph, *graph;
 
 void build_graph_cmd(pnode *head);
 void insert_node_cmd(pnode *head);
@@ -15,4 +8,3 @@ void deleteGraph_cmd(pnode* head);
 void shortsPath_cmd(pnode head);
 void TSP_cmd(pnode head);
 
-#endif
